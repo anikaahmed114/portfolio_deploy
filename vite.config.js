17e2de32c864b9fcb275c56be47d9da7209ef09d
anikaahmed114 <aahm@unc.edu>
2024-05-16 21:27:33 -0400

    progress

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio_deploy/',
-  plugins: [react()],
+  plugins: [react(), 
+    {
+      name: 'exclude-files',
+      enforce: 'post',
+      generateBundle(options, bundle) {
+        for (const fileName in bundle) {
+          if (fileName.includes('index-CL9shaaN.js') || fileName.includes('index-cGSJziIs.css')) {
+            delete bundle[fileName];
+          }
+        }
+      }
+    }],
 })
