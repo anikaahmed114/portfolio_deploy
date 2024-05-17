14fbb853deecf2340564a8402b27051e87ea119f
anikaahmed114 <aahm@unc.edu>
2024-05-16 21:35:17 -0400

    progress

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from 'vite'
+import viteCompression from 'vite-plugin-compression';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio_deploy/',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',  // Adjust the input to your main entry point
-      },
-      output: {
-        // You can also adjust output options here if needed
-      },
-      // Exclude specific files
-      external: [
-        // Add paths or file patterns to exclude
-        '/assets/index-CL9shaaN.js',
-        '/assets/index-cGSJziIs.css'
-      ]
-    }
-  }
+  plugins: [react(), viteCompression()],
 })
