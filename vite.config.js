3dbd81f7aa11c989bd1ebc128fd75900156f2bef
anikaahmed114 <aahm@unc.edu>
2024-05-16 21:29:53 -0400

    progress

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio_deploy/',
-  plugins: [react(), 
-    {
-      name: 'exclude-files',
-      enforce: 'post',
-      generateBundle(options, bundle) {
-        for (const fileName in bundle) {
-          if (fileName.includes('index-CL9shaaN.js') || fileName.includes('index-cGSJziIs.css')) {
-            delete bundle[fileName];
-          }
-        }
-      }
-    }],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx',  // Adjust the input to your main entry point
+      },
+      output: {
+        // You can also adjust output options here if needed
+      },
+      // Exclude specific files
+      external: [
+        // Add paths or file patterns to exclude
+        '/assets/index-CL9shaaN.js',
+        '/assets/index-cGSJziIs.css'
+      ]
+    }
+  }
 })
